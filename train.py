48632d3d51c1de9aa3c54ef8acdc13f9efaa5301
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2024-12-07 21:08:31 +0330

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
+
 import os
 from argparse import ArgumentParser
+from torchmetrics.classification import Accuracy
 
 import torch
 from pytorch_lightning import Trainer, seed_everything
@@ -11,9 +13,12 @@ from module import CIFAR10Module
 
 
 def main(args):
+    # ایجاد نمونه‌ای از کلاس CIFAR10Data
+    data = CIFAR10Data(args)
 
     if bool(args.download_weights):
-        CIFAR10Data.download_weights()
+        # فراخوانی متد download_weights از طریق شیء data
+        data.download_weights()
     else:
         seed_everything(0)
         os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
@@ -28,21 +33,24 @@ def main(args):
         trainer = Trainer(
             fast_dev_run=bool(args.dev),
             logger=logger if not bool(args.dev + args.test_phase) else None,
-            gpus=-1,
+            devices=1,  
             deterministic=True,
-            weights_summary=None,
             log_every_n_steps=1,
             max_epochs=args.max_epochs,
-            checkpoint_callback=checkpoint,
             precision=args.precision,
         )
 
-        model = CIFAR10Module(args)
-        data = CIFAR10Data(args)
+        
+        model = CIFAR10Module(
+            learning_rate=args.learning_rate,
+            weight_decay=args.weight_decay,
+            max_epochs=args.max_epochs,
+            classifier=args.classifier  
+        )
 
         if bool(args.pretrained):
             state_dict = os.path.join(
-                "cifar10_models", "state_dicts", args.classifier + ".pt"
+                "/content/drive/MyDrive/", "state_dicts", args.classifier + ".pt"
             )
             model.model.load_state_dict(torch.load(state_dict))
 
@@ -70,13 +78,14 @@ if __name__ == "__main__":
     parser.add_argument("--pretrained", type=int, default=0, choices=[0, 1])
 
     parser.add_argument("--precision", type=int, default=32, choices=[16, 32])
-    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--max_epochs", type=int, default=100)
-    parser.add_argument("--num_workers", type=int, default=8)
-    parser.add_argument("--gpu_id", type=str, default="3")
+    parser.add_argument("--num_workers", type=int, default=2)
+    parser.add_argument("--gpu_id", type=str, default="0")
 
     parser.add_argument("--learning_rate", type=float, default=1e-2)
     parser.add_argument("--weight_decay", type=float, default=1e-2)
 
     args = parser.parse_args()
     main(args)
+
