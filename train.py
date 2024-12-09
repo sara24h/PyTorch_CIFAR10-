92b2bcd3af630aaf0b2f95bf74d2fd2535e10e14
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2024-12-09 14:39:45 +0330

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
-
 import os
 from argparse import ArgumentParser
-from torchmetrics.classification import Accuracy
 
 import torch
 from pytorch_lightning import Trainer, seed_everything
@@ -11,14 +9,19 @@ from pytorch_lightning.loggers import WandbLogger, TensorBoardLogger
 from data import CIFAR10Data
 from module import CIFAR10Module
 
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+# ایجاد callback برای ذخیره مدل
+checkpoint_callback = ModelCheckpoint(
+    monitor="acc/val",
+    mode="max",
+    save_last=False
+)
 
 def main(args):
-    # ایجاد نمونه‌ای از کلاس CIFAR10Data
-    data = CIFAR10Data(args)
 
     if bool(args.download_weights):
-        # فراخوانی متد download_weights از طریق شیء data
-        data.download_weights()
+        CIFAR10Data.download_weights()
     else:
         seed_everything(0)
         os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
@@ -40,13 +43,14 @@ def main(args):
             precision=args.precision,
         )
 
-        
         model = CIFAR10Module(
             learning_rate=args.learning_rate,
             weight_decay=args.weight_decay,
             max_epochs=args.max_epochs,
             classifier=args.classifier  
         )
+        #model = CIFAR10Module(args)
+        data = CIFAR10Data(args)
 
         if bool(args.pretrained):
             state_dict = os.path.join(
@@ -78,14 +82,13 @@ if __name__ == "__main__":
     parser.add_argument("--pretrained", type=int, default=0, choices=[0, 1])
 
     parser.add_argument("--precision", type=int, default=32, choices=[16, 32])
-    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--max_epochs", type=int, default=100)
-    parser.add_argument("--num_workers", type=int, default=2)
-    parser.add_argument("--gpu_id", type=str, default="0")
+    parser.add_argument("--num_workers", type=int, default=8)
+    parser.add_argument("--gpu_id", type=str, default="3")
 
     parser.add_argument("--learning_rate", type=float, default=1e-2)
     parser.add_argument("--weight_decay", type=float, default=1e-2)
 
     args = parser.parse_args()
     main(args)
-
